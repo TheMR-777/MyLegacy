d4f05b30d21fe673f6d4d8ee87eb6d446aada61c
Muhammad Ammar Khan <m.shahzad.ms72@gmail.com>
2023-06-23 20:24:20 +0500

    VB: RecipeApp v7 - RDLC Report ✅

---
+++ Visual Basic (VBA)/RecipeApp v7 - RDLC Report/v1 - after/ReportApp_RDLC.vb
@@ -0,0 +1,20 @@
+﻿Public Class ReportApp_RDLC
+
+	Private Sub ReportRDLC_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+		RecipeDBTableAdapter.Fill(MyRecipeDataSet.RecipeDB)
+
+		' Formatting the Ingredients Column
+		For Each MyRow As DataRow In MyRecipeDataSet.RecipeDB.Rows
+			Dim Original = MyRow.Field(Of String)(2)
+			Dim Splitted = Original.Split(",")
+
+			If Splitted.Length > 1 Then
+				MyRow.SetField(2, String.Join(Environment.NewLine, Splitted.Select(Function(x) "• " & x.Trim())))
+			End If
+		Next
+
+		MyReportView.RefreshReport()
+
+	End Sub
+End Class
