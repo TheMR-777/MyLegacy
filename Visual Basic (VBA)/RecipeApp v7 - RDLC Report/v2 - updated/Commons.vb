107c8b6b19acf7fffb016b2727ca36653359447d
Muhammad Ammar Khan <Pakistan-786-786-786>
2023-06-24 13:42:52 +0500

    VB: RecipeApp v7 - Image: Base64 ✅

---
+++ Visual Basic (VBA)/RecipeApp v7 - RDLC Report/v2 - updated/Commons.vb
@@ -0,0 +1,22 @@
+﻿Module Utility
+	Public Function EncodeImage(ByRef image As Image) As String
+
+		If image IsNot Nothing Then
+			Using stream = New IO.MemoryStream()
+				image.Save(stream, image.RawFormat)
+				Return Convert.ToBase64String(stream.ToArray())
+			End Using
+		Else
+			Return Nothing
+		End If
+
+	End Function
+
+	Public Function DecodeImage(ByRef imageBase64 As String) As Image
+
+		Dim imageBytes = Convert.FromBase64String(imageBase64)
+		Return Image.FromStream(New IO.MemoryStream(imageBytes))
+
+	End Function
+
+End Module
