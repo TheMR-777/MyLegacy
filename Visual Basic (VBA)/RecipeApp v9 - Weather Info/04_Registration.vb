7104588366bc1016bfdc85f127d884ef122efd2d
Muhammad Ammar Khan <m.shahzad.ms72@gmail.com>
2023-06-27 19:12:11 +0500

    VB: RecipeApp v9 - Weather Information ✅

---
+++ Visual Basic (VBA)/RecipeApp v9 - Weather Info/04_Registration.vb
@@ -0,0 +1,82 @@
+﻿Public Class Registration
+
+    ' Liability
+    Public TheLogin As Login
+
+    ' Events
+    Private Sub Registration_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ' User Fields:
+        ' - F_Name
+        ' - L_Name
+        ' - Email
+        ' - Password
+
+    End Sub
+
+    Private Sub RegisterButton_Click(sender As Object, e As EventArgs) Handles RegisterButton.Click
+
+        ' Empty Field detection
+        For Each Field In {FirstName, LastName, Email, Password}
+            If Field.Text = "" Then
+                MessageBox.Show(
+                    $"The Field of {Field.Name} is empty",
+                    "Empty Field detected",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information
+                )
+                Return
+            End If
+        Next
+
+        If Not UserQuery.Register(FirstName, LastName, Email, Password) Then
+            MessageBox.Show(
+                "The Email is already registered",
+                "Duplicate detected",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information
+            )
+            Return
+        End If
+
+        ' Acknowledgement
+        MessageBox.Show(
+            "The User is registered",
+            "Registration completed",
+            MessageBoxButtons.OK,
+            MessageBoxIcon.Information
+        )
+
+        'With New RecipeApp(Email.Text)
+        '    .TheLogin = Nothing
+        '    .Register = Me
+        '    .Show()
+        'End With
+
+        With TheLogin
+            .PreLoad(Email.Text, Password.Text)
+            .Show()
+        End With
+        Close()
+
+    End Sub
+
+    ' Closers
+    Private Sub CancelButton_Click(sender As Object, e As EventArgs) Handles CancelButton.Click
+
+        Close()
+
+    End Sub
+
+    Private Sub GoLogin_Click(sender As Object, e As EventArgs) Handles GoLogin.Click
+
+        Close()
+
+    End Sub
+
+    Private Sub Registration_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+
+        TheLogin?.Show()
+
+    End Sub
+End Class
---
+++ Visual Basic (VBA)/RecipeApp v9 - Weather Info/05_Login.vb
@@ -0,0 +1,72 @@
+﻿Public Class Login
+
+	Public Sub PreLoad(ByRef Email As String, ByRef Password As String)
+
+		Me.Email.Text = Email
+		Me.Password.Text = Password
+
+	End Sub
+
+
+	Private Sub Login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+		If IsDebug Then
+			Email.Text = "mail"
+			Password.Text = "pass"
+		End If
+
+	End Sub
+
+	Private Sub Register_Click(sender As Object, e As EventArgs) Handles Register.Click
+
+		Dim Registration = New Registration() With {
+			.TheLogin = Me
+		}
+		Registration.Show()
+		Hide()
+
+	End Sub
+
+
+	Private Sub LoginButton_Click(sender As Object, e As EventArgs) Handles LoginButton.Click
+
+		For Each Field In {Email, Password}
+			If Field.Text = "" Then
+				MessageBox.Show(
+					$"The Field of {Field.Name} is empty",
+					"Empty Field detected",
+					MessageBoxButtons.OK,
+					MessageBoxIcon.Information
+				)
+				Return
+			End If
+		Next
+
+		Dim User As String = UserQuery.Login(Email, Password)
+
+		If User Is Nothing Then
+			MessageBox.Show(
+				"The Email or Password is incorrect",
+				"Incorrect Field detected",
+				MessageBoxButtons.OK,
+				MessageBoxIcon.Information
+			)
+			Return
+		End If
+
+		Dim RecipeApp = New RecipeApp(User) With {
+			.TheLogin = Me,
+			.Register = Nothing
+		}
+		RecipeApp.Show()
+		Hide()
+
+	End Sub
+
+	Private Sub CancelButton_Click(sender As Object, e As EventArgs) Handles CancelButton.Click
+
+		Close()
+
+	End Sub
+
+End Class
