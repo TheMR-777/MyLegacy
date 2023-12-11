57bdb39e4fbd8b57c2a3fcaefb5b96501e249ce5
TheMR-777 <m.shahzad.ms72@gmail.com>
2023-12-11 22:09:17 +0500

    C#: Structural Adjustments

---
+++ Avalonia/SLA Remake/10 — SLA Forever/Program.cs
@@ -0,0 +1,32 @@
+﻿using Avalonia;
+using System;
+
+namespace SLA_Remake;
+
+internal class Program
+{
+	// Initialization code. Don't use any Avalonia, third-party APIs or any
+	// SynchronizationContext-reliant code before AppMain is called: things
+	// aren't initialized yet and stuff might break.
+	[STAThread]
+	public static void Main(string[] args)
+	{
+		try
+		{
+			BuildAvaloniaApp()
+				.StartWithClassicDesktopLifetime(args);
+		}
+		catch (Exception e)
+		{
+			if (Constants.EnableLogOnDiscord)
+				WebAPI.L1_RegisterException(e);
+		}
+	}
+
+	// Avalonia configuration, don't remove; also used by visual designer.
+	public static AppBuilder BuildAvaloniaApp()
+		=> AppBuilder.Configure<App>()
+			.UsePlatformDetect()
+			.WithInterFont()
+			.LogToTrace();
+}
