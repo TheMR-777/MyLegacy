fa3cae5c6dd94e39e82e093d09b4b5849eaca3f2
Muhammad Ammar <m.shahzad.ms72@gmail.com>
2024-06-28 20:55:48 +0500

    Rust: Integers

---
+++ Rust/Variables — Integers/01_ints.rs
@@ -0,0 +1,11 @@
+fn main()
+{
+    // Integers in Rust
+
+    let _8: i8 = 8;
+    let _16: i16 = 16;
+    let _32: i32 = 32;
+    let _64: i64 = 64;
+    let _128: i128 = 128;
+    let _size: isize = 256;
+}
