0e682418e7aaf173511db0b584d11ffd6e5c1c69
Muhammad Ammar <m.shahzad.ms72@gmail.com>
2024-06-28 16:15:05 +0500

    Rust: Bismillah

---
+++ Rust/@Hello-World/01_getting_started.rs
@@ -0,0 +1,6 @@
+fn main()
+{
+    println!("Hi, it's TheMR");
+    println!("I just started learning Rust, while the \"SLA-Remake: Dashboard\" is being deployed :)");
+    println!("- June 28th, 2024");
+}
